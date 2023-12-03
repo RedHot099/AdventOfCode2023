4daa51372f68983541ac9d0ec97cbb465f9784b3
Kuba <redhotdevil996@gmail.com>
2023-12-03 10:46:39 +0100

    day1 part 2 solution

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -7,13 +7,9 @@ pub fn main() !void {
 
 
 pub fn part1() !void {
-    var file = try std.fs.cwd().openFile("input.txt", .{});
-    defer file.close();
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-    var buf: [1024]u8 = undefined;
+    var lines_iter = std.tokenizeScalar(u8, @embedFile("input.txt"), '\n');
     var sum: u32 = 0;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while (lines_iter.next()) |line| {
         var first:u32 = 0;
         var last:u32 = 0;
         for (line) |c| {
@@ -34,4 +30,38 @@ pub fn part1() !void {
 
 pub fn part2() !void {
     const numbers = [_][]const u8{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    var lines_iter = std.tokenizeScalar(u8, @embedFile("input.txt"), '\n');
+    var sum:u32 = 0;
+
+    while (lines_iter.next()) |line| {
+        if (line.len == 0) continue;
+        var first:u32 = null;
+        var last:u32 = null;
+        next_digit: for (0..line.len) |i| {
+            if (std.ascii.isDigit(line[i])) {
+                if (first == 0){
+                    first = c - '0';
+                    last = first;
+                } else {
+                    last = c - '0';
+                }
+                continue;
+            }
+            for(numbers, 1..) |number, digit| {
+                if (i + number.len - 1 < line.len & std.mem.eql(u8, line[i..i+number.len], number)) {
+                    if (first == 0){
+                        first = @intCast(digit);
+                        last = first;
+                    } else {
+                        last = @intCast(digit);
+                    }
+                    continue :next_digit;
+                }
+            }
+        }
+        std.debug.assert(first != null);
+        sum += first*10+last;
+    }
+    print("{}\n", .{sum});
 }
